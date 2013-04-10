41fbbab618be8ef3492de91339c3c9604fff9bcf
Andrew Morgan <anoadragon453@gmail.com>
2013-04-09 18:59:43 -0700

    Initial Commit

---
+++ iTutor-Remake/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  iTutor-Remake
+//
+//  Created by Andrew Morgan on 4/9/13.
+//  Copyright (c) 2013 Andrew Morgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
